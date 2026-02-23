6a34a866b8975165a9575dc6b3c7ae57333dd051
Gabriel V <gaboxv@gmail.com>
2026-02-23 22:34:50 +0100

    Add minimal Neovim config (practice setup)

---
+++ nvimconfig/.config/nvim/init.lua
@@ -0,0 +1,20 @@
+-- Minimal Neovim config for learning Vim
+
+-- Line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Indentation
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+-- Better search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Keep cursor centered
+vim.opt.scrolloff = 8
+
+-- Enable mouse (optional)
+vim.opt.mouse = "a"
